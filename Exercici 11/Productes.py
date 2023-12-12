2d632ea4179d179ad1eecf26298acf1196271d7a
Pumpunkio <martinicollvi@gmail.com>
2023-12-12 19:10:29 +0100

    Classe producte i test producte

---
+++ Exercici 11/Productes.py
@@ -0,0 +1,37 @@
+class Producte():
+    """
+    Class Producte: representen els productes que ofereixen els proveïdors.
+    Mètodes:
+        __init__: inicialització dels atributs de l'instància
+        Valora: Afegir una valoració al producte
+
+    """
+
+    def __init__(self, codi, nom, preu, descripcio):
+        try:
+            preu=float(preu)
+        except:
+            raise ValueError("El valor del preu és incorrecte")
+        else:
+            self.codi = codi
+            self.nom = nom
+            self.preu = preu
+            self.descripcio = descripcio
+            self.valoracions = []
+
+    def __str__(self):
+        return ("INFORMACIÓ DEL PRODUCTE\nCodi: " + str(self.codi) + "\nNom: " + self.nom + "\nPreu: " +
+                str(self.preu) + "\nDescripció: " + self.descripcio + "\nValoracions:\n\t" +
+                ('\n\t'.join(', '.join(map(str, sublist)) for sublist in self.valoracions)))
+
+    def __iter__(self):
+        return self.valoracions.__iter__()
+
+    def valora(self, nom_client, puntuacio, opinio):
+        try:
+            assert(puntuacio==int(puntuacio))
+            assert(0<puntuacio<6)
+        except:
+            raise ValueError("El valor de la valoració és incorrecte")
+        else:
+            self.valoracions.append([nom_client, puntuacio, opinio])
---
+++ Exercici 11/Productes_test.py
@@ -0,0 +1,41 @@
+from Productes import Producte
+import sys
+
+print("=================================")
+print("TEST CLASS PRODUCTE:")
+print("=================================")
+
+print("TEST 1: CREAR PRODUCTES QUE PUGUIN EXISTIR\nResultat:", end= " ")
+pr1_codi = "0000A"
+pr2_nom = "samarreta"
+pr3_preu = 15.99
+pr4_des = "Descripció sobre jaqueta"
+try:
+    pr1 = Producte(pr1_codi, "pantalons", "12.99", "Descripció sobre pantalons")
+    pr2 = Producte("0000B", pr2_nom, 13, "Descripció sobre samarreta")
+    pr3 = Producte("0000C", "camisa", pr3_preu, "Descripció sobre camisa")
+    pr4 = Producte("0000D", "jaqueta", 20 + 1.99, pr4_des)
+
+    # Comprovem que els diversos atributs estiguin ben implementats dintre les instàncies
+    assert(pr1.codi==pr1_codi)
+    assert(pr2.nom==pr2_nom)
+    assert(pr3.preu==pr3_preu)
+    assert(pr4.descripcio==pr4_des)
+except:
+    print("Productes no creats [ERROR]")
+    sys.exit()
+else:
+    print("Productes creats correctament [OK]")
+
+print("=================================")
+print("TEST 2: CREAR UN FALS PRODUCTE\nResultat:", end= " ")
+try:
+    prf = Producte("0001A", "Pantalons falsos", "preu evidentment fals", "Descripció falsa")
+except:
+    print("Producte no creat [OK]")
+else:
+    print("Producte creat [ERROR]")
+    sys.exit()
+
+print("=================================")
+print("TEST 2: CREAR UN FALS PRODUCTE\nResultat:", end= " ")
